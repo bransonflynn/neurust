db99f9e6f770eea32ec659ea43200cecd49cb96a
bp42s <bransonyflynn@gmail.com>
2024-10-30 11:13:52 -0400

    attempted to work with rust numpy, not functional

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ pub mod neurust;
 #[allow(dead_code)]
 #[allow(unused_variables)]
 fn main() {
-    let n1: neurust::Neuron = neurust::make_neuron((2, 3), 4);
+    let n1: neurust::Neuron = neurust::make_neuron(vec![2, 3], 4);
 }
--- src/neurust.rs
+++ src/neurust.rs
@@ -1,14 +1,34 @@
+#[allow(unused_imports)]
+use numpy::dot_bound;
+
 #[allow(dead_code)]
 pub struct Neuron {
-    weights: (i32, i32),
+    weights: Vec<i32>,
     bias: i32,
 }
 #[allow(dead_code)]
 #[allow(unused_variables)]
 impl Neuron {
-    pub fn feed_forward(&self, inputs: (i32, i32)) {
-        //
+    pub fn feed_forward(&self, inputs: Vec<i32>) {
+        //let total = dot_bound(self.weights, input);
     }
+    /*
+    def feedforward(self, inputs):
+        # Weight inputs, add bias, then use the activation function
+        total = np.dot(self.weights, inputs) + self.bias
+        return sigmoid(total)
+    */
+}
+
+pub fn make_neuron(weights: Vec<i32>, bias: i32) -> Neuron {
+    let result: Neuron = Neuron { weights, bias };
+    result
+}
+
+#[allow(dead_code)]
+#[allow(unused_variables)]
+pub fn sigmoid(x: f64) -> f64 {
+    1.0 / (1.0 + (-x).exp())
 }
 
 #[allow(dead_code)]
@@ -18,18 +38,3 @@ pub struct NeuralNetwork {
 }
 impl NeuralNetwork {}
 
-pub fn make_neuron(weights: (i32, i32), bias: i32) -> Neuron {
-    let result: Neuron = Neuron { weights, bias };
-    result
-}
-
-#[allow(dead_code)]
-#[allow(unused_variables)]
-pub fn sigmoid(x: f64) -> f64 {
-    1.0 / (1.0 + (-x))
-}
-
-#[allow(unused_variables)]
-pub fn exp(input: &mut (i32, i32)) {
-    //utils::math::sigmoid(0.6)
-}
