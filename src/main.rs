aa65520c680afc210d7e06c23a199f2a6e7a4050
bp42s <bransonyflynn@gmail.com>
2024-10-30 09:10:48 -0400

    added neurust file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub mod neurust;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/neurust.rs
@@ -0,0 +1,16 @@
+#[allow(dead_code)]
+pub struct Neuron {
+    weights: (i32, i32),
+    bias: i32,
+}
+#[allow(dead_code)]
+#[allow(unused_variables)]
+impl Neuron {
+    pub fn feed_forward(&self, inputs: (i32, i32)) {
+        //
+    }
+
+    pub fn sigmoid(x: i32) {
+        //
+    }
+}
