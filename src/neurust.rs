c27ea8b0496ff61b05ba384528b7030ac370c5fd
bp42s <bransonyflynn@gmail.com>
2024-10-30 16:02:15 -0400

    added NeuralNetwork impl funcs

--- src/neurust.rs
+++ src/neurust.rs
@@ -1,8 +1,11 @@
-#[allow(unused_imports)]
-use numpy::dot_bound;
+//static mut ID_COUNTER_NEURON: u64 = 0;
+//static mut ID_COUNTER_NEURALNETWORK: u64 = 0;
+
+use std::collections::HashMap;
 
 #[allow(dead_code)]
 pub struct Neuron {
+    id: u64,
     weights: Vec<i32>,
     bias: i32,
 }
@@ -12,29 +15,42 @@ impl Neuron {
     pub fn feed_forward(&self, inputs: Vec<i32>) {
         //let total = dot_bound(self.weights, input);
     }
-    /*
-    def feedforward(self, inputs):
-        # Weight inputs, add bias, then use the activation function
-        total = np.dot(self.weights, inputs) + self.bias
-        return sigmoid(total)
-    */
 }
 
-pub fn make_neuron(weights: Vec<i32>, bias: i32) -> Neuron {
-    let result: Neuron = Neuron { weights, bias };
+pub fn make_neuron(a_weights: Vec<i32>, a_bias: i32) -> Neuron {
+    let result: Neuron = Neuron {
+        id: 0, // todo setup static counter for ID
+        weights: a_weights,
+        bias: a_bias,
+    };
     result
 }
 
 #[allow(dead_code)]
 #[allow(unused_variables)]
 pub fn sigmoid(x: f64) -> f64 {
+    // todo - doesnt work
     1.0 / (1.0 + (-x).exp())
 }
 
 #[allow(dead_code)]
 pub struct NeuralNetwork {
     id: u64,
-    neurons: Vec<Neuron>,
+    neurons_map: HashMap<u64, Neuron>,
 }
-impl NeuralNetwork {}
+impl NeuralNetwork {
+    pub fn add_neuron(&mut self, neur: Neuron) {
+        self.neurons_map.insert(neur.id, neur);
+    }
 
+    pub fn get_neuron(&self, id: u64) -> Option<&Neuron> {
+        if self.neurons_map.contains_key(&id) {
+            let result= self.neurons_map.get(&id);
+            match result {
+                Some(neur) => return Some(neur),
+                None => return None,
+            }
+        }
+        None
+    }
+}
